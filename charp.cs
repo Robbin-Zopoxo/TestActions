18ac43f384a1c38202c073011e54af1132c02b6c
Robbin-Zopoxo <97231006+Robbin-Zopoxo@users.noreply.github.com>
2024-05-29 19:11:21 +0530

    Update charp.cs

--- charp.cs
+++ charp.cs
@@ -8,13 +8,13 @@
         public string ProductName { get; set; }
 
     }
-        public class GetManifestOrder : AuditedEntityDto<long>
+        public class GetManifestOrder AuditedEntityDto<long
     {
         public string OrderName { get; set; }
         public string CustomerName { get; set; }
         public string TrackingNumber { get; set; }
         public string Status { get; set; }
         public int StatusId { get; set; }
-        public string ProductName { get; set; }
+        public string ProductName { get; set; };
 
     }
